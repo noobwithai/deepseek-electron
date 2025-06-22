952b8d0a76d647e9b2f9c95499c5cb181a3231cb
noobwithai <216304469+noobwithai@users.noreply.github.com>
2025-06-22 12:18:05 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+process.env.ELECTRON_DISABLE_GTK = 'true';
+const { app, BrowserWindow } = require('electron')
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  win.loadURL('https://chat.deepseek.com')
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+})
